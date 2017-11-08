bc8369525bb737930fc2ad3ec4dd867aaacf936b
pranavj1001 <pranavj1001@gmail.com>
2017-11-08 14:55:24 +0530

    delete session data after user logs out

--- TeenHub/movies/urls.py
+++ TeenHub/movies/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^logout$', views.logout, name='logout'),
     url(r'^$', views.show_movies, name='show_movies_page'),
 ]
--- TeenHub/movies/views.py
+++ TeenHub/movies/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def logout(request):
+    print("deleted session")
+    del request.session["id"]
+    return render(request, 'home/home.html', {})
+
 def show_movies(request):
     return render(request, 'movies/session.html', {})
+
