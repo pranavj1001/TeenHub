690deaea06bb69aa92b2840a51c7f6c911539c4e
pranavj1001 <pranavj1001@gmail.com>
2017-11-12 15:04:39 +0530

    add url for view movie info

--- TeenHub/movies/urls.py
+++ TeenHub/movies/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^logout$', views.logout, name='logout'),
+    url(r'^infoMovies/(?P<movieid>\d+)/$', views.show_movie_info, name='show_movie_info'),
     url(r'^$', views.show_movies, name='show_movies_page'),
 ]
--- TeenHub/movies/views.py
+++ TeenHub/movies/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render, redirect
 def logout(request):
     print("deleted session")
     del request.session["id"]
+    del request.session["movieid"]
     return render(request, 'home/home.html', {})
 
 def show_movies(request):
     return render(request, 'movies/session.html', {})
 
+def show_movie_info(request, movieid):
+    request.session["movieid"] = movieid
+    return render(request, 'movies/viewInfoMovies.html', {})
