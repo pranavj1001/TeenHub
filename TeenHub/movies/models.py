43d1ca685185a6d452ff38f16a7e027549c37a86
pranavj1001 <pranavj1001@gmail.com>
2017-12-16 12:51:53 +0530

    make migrations for the links movies table

--- TeenHub/movies/models.py
+++ TeenHub/movies/models.py
@@ -5,3 +5,7 @@ class Ratings(models.Model):
     user_id = models.IntegerField(blank=False)
     movie_id = models.IntegerField(blank=False)
     ratings = models.DecimalField(max_digits=2, decimal_places=1, blank=False)
+
+class Links(models.Model):
+    movie_id = models.IntegerField(blank=False)
+    tmdb_id = models.IntegerField(blank=False)
