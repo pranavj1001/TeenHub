b143d981f28456ab97a73b67b1478536f14ba14e
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-12-25 23:11:38 +0530

    adding searchBar on session_news.html

--- TeenHub/news/views.py
+++ TeenHub/news/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_news(request):
-	return render(request, 'news/news.html', {})
+	return render(request, 'news/session_news.html', {})
