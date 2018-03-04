90991209006b318a191ea4a513c63df22e09e99b
kedarkale <kedar30092010@gmail.com>
2018-03-04 14:09:08 +0530

    change games icon quality and start work on infoGames

--- TeenHub/games/urls.py
+++ TeenHub/games/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.games_home, name='show_games_page'),
+    url(r'^infoGames/(?P<gameid>\d+)/$', views.show_game_info, name='show_game_info'),
 ]
--- TeenHub/games/views.py
+++ TeenHub/games/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def games_home (request):
 	return render(request,'games/session_games.html')
+
+def show_game_info(request, gameid):
+	return render(request, 'games/viewInfoGames.html', {})
