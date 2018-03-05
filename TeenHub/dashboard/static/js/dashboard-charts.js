10c6329e1778f97a479c410629673fb3fe7fb3a3
pranavj1001 <pranavj1001@gmail.com>
2018-03-06 00:15:24 +0530

    add a static pie chart
    this pie chart shows the number of movies, songs and games

--- TeenHub/dashboard/static/js/dashboard-charts.js
+++ TeenHub/dashboard/static/js/dashboard-charts.js
@@ -44,15 +44,3 @@ var myLineChart = new Chart(ctx, {
     }
   }
 });
-// -- Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
-    datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
-    }],
-  },
-});
