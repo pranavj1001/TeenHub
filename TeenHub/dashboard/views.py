ee230c561978ddc6e52853bf5829897eaf6d5a8d
pranavj1001 <pranavj1001@gmail.com>
2018-03-05 22:14:47 +0530

    set the structure of the dashboard components

--- TeenHub/dashboard/views.py
+++ TeenHub/dashboard/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def show_dashboard(request):
-    return render(request, 'dashboard/dashboard.html', {})
+    return render(request, 'dashboard/dashboard_home.html', {})
