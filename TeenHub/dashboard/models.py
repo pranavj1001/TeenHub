988341186a3c17de283b1d1676bd5343fe63ae78
pranavj1001 <pranavj1001@gmail.com>
2018-03-07 11:26:56 +0530

    add model dashboard_feed

--- TeenHub/dashboard/models.py
+++ TeenHub/dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class feed(models.Model):
+    createdBy = models.IntegerField(blank=False)
+    message = models.CharField(max_length=200, blank=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    comments = models.IntegerField(blank=False, default=0)
