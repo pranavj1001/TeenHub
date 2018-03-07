b33329803e96849f0789ef43f611f9df6fddb9de
pranavj1001 <pranavj1001@gmail.com>
2018-03-07 11:12:56 +0530

    show total number of users, visits and current version number

--- TeenHub/dashboard/views.py
+++ TeenHub/dashboard/views.py
@@ -6,11 +6,17 @@ from datetime import date
 
 def show_dashboard(request):
     last_ten = visitors.objects.all().order_by('-id')[:10][::-1]
+    all_records = visitors.objects.all()
     visits_month = []
     month_names = []
     signups_month = []
     max_lineGraph = 0
     max_barGraph = 0
+    total_visits = 0
+    total_users = 0
+    for i in range(0, len(all_records)):
+        total_visits += all_records[i].visits
+        total_users += all_records[i].signups
     for i in range(0, len(last_ten)):
         # print(last_ten[i].visits, last_ten[i].year, last_ten[i].month)
         visits_month.append(last_ten[i].visits)
@@ -26,4 +32,14 @@ def show_dashboard(request):
 
     print(signups_month[-6:], month_names[-6:], max_lineGraph, max_barGraph)
 
-    return render(request, 'dashboard/dashboard_home.html', {"visits_month": visits_month, "signups_month": signups_month[-6:], "signup_month_names": month_names[-6:], "month_names": month_names, "max_lineGraph": max_lineGraph, "max_barGraph": max_barGraph})
+    return render(request, 'dashboard/dashboard_home.html',
+                  {
+                      "visits_month": visits_month,
+                       "signups_month": signups_month[-6:],
+                       "signup_month_names": month_names[-6:],
+                       "month_names": month_names,
+                       "max_lineGraph": max_lineGraph,
+                       "max_barGraph": max_barGraph,
+                       "total_visits": total_visits,
+                       "total_users": total_users
+                   })
