f59a6fc980510f3f309014de8de5b5c988785db9
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2018-03-13 00:25:30 +0530

    displaying searchBox in songs section if session id is set

--- TeenHub/songs/views.py
+++ TeenHub/songs/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_songs(request):
+    if 'id' in request.session:
+        return render(request, 'songs/session.html',{})
     return render(request, 'songs/songs.html', {})
