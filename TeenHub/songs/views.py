2de290b88c60157ad059f3ab7f4df3c9c02fc24c
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2018-03-20 20:15:38 +0530

    updating view in songs

--- TeenHub/songs/views.py
+++ TeenHub/songs/views.py
@@ -4,13 +4,14 @@ from django.shortcuts import render
 def show_songs(request):
     return render(request, 'songs/session.html',{})
 
-def show_song_info(request,songsinfo,type):
+def show_song_info(request,songsinfo,name,type):
+
     if type == "1":
         print("artist")
-        return render(request, 'songs/viewInfoArtistSongs.html', {'songsinfo':songsinfo,'type':type})
+        return render(request, 'songs/viewInfoArtistSongs.html', {'songsinfo':songsinfo,'name':name,'type':type})
     elif type == "2":
         print("tracks")
-        return render(request, 'songs/viewInfoTrackSongs.html', {'songsinfo':songsinfo,'type':type})
-    else:
+        return render(request, 'songs/viewInfoTrackSongs.html', {'songsinfo':songsinfo,'name':name,'type':type})
+    elif type == "3":
         print("albums")
-        return render(request, 'songs/viewInfoAlbumSongs.html', {'songsinfo':songsinfo,'type':type})
+        return render(request, 'songs/viewInfoAlbumSongs.html', {'songsinfo':songsinfo,'name':name,'type':type})
