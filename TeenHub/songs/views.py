bdd9e88a15db58a751ff32e8b9c6453bc8eb6968
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2018-03-20 16:15:30 +0530

    displaying artist on new page

--- TeenHub/songs/views.py
+++ TeenHub/songs/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_songs(request):
-    if 'id' in request.session:
-        return render(request, 'songs/session.html',{})
-    return render(request, 'songs/songs.html', {})
+    return render(request, 'songs/session.html',{})
 
 def show_song_info(request,songsinfo,type):
-    return render(request, 'songs/viewInfoSongs.html', {'songsinfo':songsinfo,'type':type})
+    if type == "1":
+        print("artist")
+        return render(request, 'songs/viewInfoArtistSongs.html', {'songsinfo':songsinfo,'type':type})
+    elif type == "2":
+        print("tracks")
+        return render(request, 'songs/viewInfoTrackSongs.html', {'songsinfo':songsinfo,'type':type})
+    else:
+        print("albums")
+        return render(request, 'songs/viewInfoAlbumSongs.html', {'songsinfo':songsinfo,'type':type})
